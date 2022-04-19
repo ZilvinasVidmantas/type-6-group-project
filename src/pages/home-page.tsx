e99624d96a278402ed831703ea7b49082869a309
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-19 10:52:25 +0300

    data fetch

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 import {
   Container,
   Box,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+  Button,
 } from '@mui/material';
 import { User } from '../types';
 import UsersService from '../services/users-service';
@@ -9,23 +17,62 @@ import UsersService from '../services/users-service';
 const HomePage: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
 
-  // Vykdysis tik pirmo užkrovimo metu
-  useEffect(() => {
-    (async () => {
-      // Parsiunčiami duomenys
-      const fetchedUsers = await UsersService.fetchUsers();
-      //  Po parsiuntimo atnaujinas users state kintamasis
-      setUsers(fetchedUsers);
-    })();
-  }, []);
+  const fetchUsers = async (): Promise<void> => {
+    const fetchedUsers = await UsersService.fetch();
+    setUsers(fetchedUsers);
+  };
 
-  console.log('Komponentas kraunasi');
-  console.log({ users });
+  const deleteUser = async (id: string) => {
+    await UsersService.delete(id);
+    fetchUsers();
+  };
+
+  useEffect(() => {
+    // Vykdysis tik pirmo užkrovimo metu
+    fetchUsers();
+  }, []);
 
   return (
     <Container sx={{ my: 5 }}>
       <Box component="pre">
-        {JSON.stringify(users, null, 2)}
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Id</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Phone</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users.map(({
+                id,
+                name,
+                phone,
+                email,
+              }) => (
+                <TableRow key={id}>
+                  <TableCell>{id}</TableCell>
+                  <TableCell>{name}</TableCell>
+                  <TableCell>{phone}</TableCell>
+                  <TableCell>{email}</TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      color="error"
+                      size="small"
+                      onClick={() => deleteUser(id)}
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </Container>
   );
--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -1,41 +1,19 @@
 import { User } from '../types';
 
-const pause = (ms: number): Promise<void> => new Promise((res) => { setTimeout(() => res(), ms); });
-
 const UsersService = new (class UsersService {
   // eslint-disable-next-line class-methods-use-this
-  public fetchUsers = async (): Promise<User[]> => {
-    await pause(5000);
-    return [{
-      id: 'BCD037CB-D642-6557-9795-BAAEB25AE2D9',
-      name: 'Hanae Olson',
-      phone: '(247) 325-6454',
-      email: 'mauris@yahoo.com',
-    },
-    {
-      id: '1CABB1E7-8C7E-7702-AE5A-01A6DB7F6239',
-      name: 'Yen Hess',
-      phone: '1-512-675-8383',
-      email: 'at@hotmail.org',
-    },
-    {
-      id: '2C0F8695-85D8-9A14-411C-EA579AD4D5D4',
-      name: 'Xantha Rios',
-      phone: '(358) 848-5765',
-      email: 'amet.metus@icloud.net',
-    },
-    {
-      id: 'DA89A964-7ECF-3AFA-C297-6285AD7C2D98',
-      name: 'Joy Crosby',
-      phone: '1-620-635-9268',
-      email: 'sit.amet.massa@google.com',
-    },
-    {
-      id: 'DB81BA3E-EA41-8551-7232-D2966AA9BC6D',
-      name: 'Amal Edwards',
-      phone: '1-752-892-3749',
-      email: 'ut.nulla@aol.ca',
-    }];
+  public fetch = async (): Promise<User[]> => {
+    const response = await fetch('http://localhost:5000/users');
+    const users = await response.json() as User[];
+
+    return users;
+  };
+
+  // eslint-disable-next-line class-methods-use-this
+  public delete = async (id: string): Promise<void> => {
+    await fetch(`http://localhost:5000/users/${id}`, {
+      method: 'DELETE',
+    });
   };
 })();
 
