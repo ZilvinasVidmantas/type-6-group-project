b52f04269b0eb4600bfdb500dcb65addb41a2e52
edvinaspetra <edvinas.petra@gmail.com>
2022-04-20 16:25:53 +0300

    feat: created home page structure

---
+++ src/pages/home-page/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePageLandingSection from './landing-section';
+import HomePageInfoSection from './info-section/index';
+
+const HomePage: React.FC = () => (
+  <>
+    <HomePageLandingSection />
+    <HomePageInfoSection />
+  </>
+
+);
+
+export default HomePage;
