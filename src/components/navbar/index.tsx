940b283dffb6c1d0351def3809706f39f841b541
Andrius <96168393+aapocius@users.noreply.github.com>
2022-04-21 10:17:17 +0300

    feat/ navbar-icon-navlink-add

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,17 +4,21 @@ import {
   Container,
   Box,
 } from '@mui/material';
+
 import NavbarLink from './navbar-link';
 import Logo from '../logo';
 
 const Navbar: React.FC = () => (
   <AppBar position="static" sx={{ bgcolor: 'grey.900' }}>
     <Container sx={{ px: { xs: 0, sm: 0 } }}>
-      <Box sx={(theme) => theme.mixins.navbar}>
+      <Box
+        sx={(theme) => theme.mixins.navbar}
+      >
         <Logo size={40} />
         <Box sx={{ alignSelf: 'stretch' }}>
-          <NavbarLink to="/">Home</NavbarLink>
-          <NavbarLink to="/buttons">Buttons</NavbarLink>
+          <NavbarLink to="/">FOOD</NavbarLink>
+          <NavbarLink to="/buttons">ENTERTAINMENT</NavbarLink>
+          <NavbarLink to="/buttons">CULTURE</NavbarLink>
         </Box>
       </Box>
     </Container>
