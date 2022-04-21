b2932b3ae3082eaedb61ebbd1e7f944ec6a2e318
MildaPau <pauzaite.milda@gmail.com>
2022-04-21 18:21:09 +0300

    feat: landing page title

--- src/pages/home-page/landing-section/index.tsx
+++ src/pages/home-page/landing-section/index.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import CustomContainer from '../../../components/container/custom-container';
 
 const HomePageLandingSection: React.FC = () => (
   <CustomContainer>
-    <Typography component="h1" variant="h1">Home Page Landing Section</Typography>
+    <Box component="section" sx={{ height: '100vh', textAlign: 'center' }}>
+      <Typography
+        fontWeight="400"
+        component="h1"
+        variant="h1"
+        sx={{
+          display: 'inline-block',
+          borderBottom: 2,
+          borderColor: 'common.black',
+          color: 'primary.main',
+          fontSize: '110px',
+          fontWeight: 700,
+          pt: 13,
+          pb: 1,
+          px: 12,
+          mb: 2,
+          textShadow: '0px 0px 8px #141313',
+        }}
+      >
+        VILNIUS
+      </Typography>
+      <Typography
+        fontWeight="400"
+        component="h1"
+        variant="h3"
+        sx={{
+          fontWeight: 400,
+        }}
+      >
+        Explore together!!
+      </Typography>
+    </Box>
   </CustomContainer>
 );
 
