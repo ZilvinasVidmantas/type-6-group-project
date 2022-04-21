f6b2f20fa84be7c3aea6468830998f520b04f440
kraubunas <kraubun@gmail.com>
2022-04-21 14:37:47 +0300

    feat: added food category page

---
+++ src/pages/food-category-page/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FoodCategoryPage: React.FC = () => (
+  <>
+    <div>FoodCategoryPage</div>
+    <h1>Food category page</h1>
+  </>
+);
+
+export default FoodCategoryPage;
