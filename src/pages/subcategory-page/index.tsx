4103d3237a020f41989663d928fa0dfdd20d9141
isliuzinskaite <indre.sliuzinskaite@gmail.com>
2022-04-21 19:00:31 +0300

    Feat: card layout, expandable description, button to location (not functional)

--- src/pages/subcategory-page/index.tsx
+++ src/pages/subcategory-page/index.tsx
@@ -10,18 +10,21 @@ const meatPlaces = [
     title: 'Restaurant Lokys',
     photo: 'https://www.tourtravelhotels.com/wp-content/uploads/2021/05/Restaurant-Lokys.jpg',
     location: 'Stikliu str. 8, Vilnius',
+    description: 'Lithuanian restaurant in Vilnius Old Town is a second-generation family restaurant, which specializes in historical traditional Lithuanian cuisine. Our menu narrates olden stories of Lithuanian noblemen, who feasted on forest foods like berries, mushrooms, nuts, and game.',
   },
   {
     id: '2',
     title: 'Bistro 18',
     photo: 'https://media-cdn.tripadvisor.com/media/photo-s/01/ed/ea/00/bistro-18.jpg',
     location: 'Stiklių str. 18, Vilnius',
+    description: 'At bistro 18 we have a passion for and commitment to food. Our driving force is a need to provide the very best dishes with the best of care and the least intrusion to the products themselves. We pride ourselves on making everything in house, something which requires more time and patience but from which the end result sings.',
   },
   {
     id: '3',
     title: 'Meat Lovers Pub',
     photo: 'https://media-cdn.tripadvisor.com/media/photo-s/02/c5/3a/88/meat-lovers-pub.jpg',
     location: 'Šv.Ignoto g. 14, Vilnius',
+    description: '#6 of 44 steak restaurants in Vilnius #41 of 146 BBQs in Vilnius Add a photo 20 photos Besides Statue of Laurynas Gucevicius, visit Meat Lovers in the vicinity. At this restaurant, visitors may have good medium steaks, hamburgers and pork ribs. You will enjoy its food, particularly tasty chocolate cakes, cheesecakes and crème brûlée.',
   },
 ];
 
@@ -31,8 +34,8 @@ const SubcategoryPage: React.FC = () => {
   return (
     <CustomContainer>
       {meatPlaces.map(({
-        title, photo, location, id,
-      }) => (<SubcategoryPageCard id={id} key={id} photo={photo} title={title} location={location} />))}
+        title, photo, location, id, description,
+      }) => (<SubcategoryPageCard id={id} key={id} photo={photo} title={title} location={location} description={description} />))}
     </CustomContainer>
   );
 };
--- src/pages/subcategory-page/subcategory-page-card.tsx
+++ src/pages/subcategory-page/subcategory-page-card.tsx
@@ -1,75 +1,107 @@
 import {
-  Card, CardContent, CardMedia, Typography, Paper,
+  Box, Card, CardActionArea, CardContent, CardMedia, Typography,
 } from '@mui/material';
 import React from 'react';
 import expand from '../../helpers/subcategory-page-card-expander';
+import Button1 from '../../components/subcategory-button';
 
 type SubcategoryPageCardProps = {
   id: string,
   title: string,
   photo: string,
   location: string,
+  description: string,
+};
+
+const expand2 = (e: any) => {
+  const element = e.target;
+  if (!element) return;
+  const card = element.closest('.MuiCard-root');
+  card.classList.toggle('expanded');
 };
 
 const SubcategoryPageCard: React.FC<SubcategoryPageCardProps> = ({
-  title, photo, location, id,
+  title, photo, location, id, description,
 }) => (
-  <Paper
-    onClick={(e) => expand(e)}
-    id={id}
-    className="card"
-    elevation={4}
-    sx={{
-      p: 6,
-
-      '&.expand': {
-        height: 600,
-        width: '500px',
-        justifySelf: 'center',
-        position: 'absolute',
-        top: 100,
-      },
-      '&.hide': {
-        opacity: 0,
-        transform: 'translateY(820px)',
-      },
-    }}
+  // <Paper
+  //   onClick={(e) => expand(e)}
+  //   id={id}
+  //   className="card"
+  //   elevation={4}
+  //   sx={{
+  //     p: 6,
+  //     '&.expand': {
+  //       height: 600,
+  //       width: '500px',
+  //       justifySelf: 'center',
+  //       position: 'absolute',
+  //       top: 100,
+  //     },
+  //     '&.hide': {
+  //       opacity: 0,
+  //       transform: 'translateY(820px)',
+  //     },
+  //   }}
+  // >
+  <Card sx={{
+    marginTop: '2rem',
+    width: '300px',
+    border: 'none',
+    boxShadow: 'none',
+    borderRadius: '2px',
+    '&:hover': {
+      transform: 'scale(1.1) perspective(0px)',
+      transition: 'transform .2s ease-out',
+    },
+    '&.expanded .description': {
+      display: 'block',
+    },
+  }}
   >
-    <Card sx={{
-      border: 'none',
-      boxShadow: 'none',
-      borderRadius: '2px',
-      '&:hover': {
-        transform: 'scale(1.1) perspective(0px)',
-        transition: 'transform .2s ease-out',
-      },
-    }}
-    >
+    <CardActionArea onClick={(e) => expand2(e)}>
       <CardMedia component="img" height="120" image={photo} />
       <CardContent>
         <Typography
           component="h2"
-          variant="h4"
+          variant="h5"
           sx={{
             textAlign: 'center',
             textTransform: 'uppercase',
-            //   fontFamily: 'Cinzel Decorative',
           }}
         >
           {title}
         </Typography>
         <Typography
           component="h3"
-          variant="h5"
+          variant="body1"
           sx={{
             textAlign: 'center',
           }}
         >
           {location}
         </Typography>
+        <Box
+          className="description"
+          sx={{
+            display: 'none',
+            paddingTop: '1rem',
+          }}
+        >
+          <Button1 />
+          <Typography
+            component="p"
+            variant="body2"
+            sx={{
+              marginTop: '1rem',
+              textAlign: 'justify',
+            }}
+          >
+            {description}
+          </Typography>
+        </Box>
       </CardContent>
-    </Card>
-  </Paper>
+    </CardActionArea>
+  </Card>
 );
 
 export default SubcategoryPageCard;
