37682429ea11fd87b02bf1c4bcba3205722066d5
Rokas <jazbutisrokas@gmail.com>
2022-04-21 12:21:34 +0300

    a-task-4: container-created

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import HomePageLandingSection from './landing-section';
 import HomePageInfoSection from './info-section';
+import CustomContainer from '../../components/container/custom-container';
 
 const HomePage: React.FC = () => (
-  <>
+  <CustomContainer>
     <HomePageLandingSection />
     <HomePageInfoSection />
-  </>
+  </CustomContainer>
 
 );
 
